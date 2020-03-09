968fd62d5fad5d3c4024f6b4031860825a3290f7
Emil Ekberg <ekberg.emil@gmail.com>
2020-03-09 23:03:57 +0100

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,36 @@
 import typescript from '@rollup/plugin-typescript';
 
-const packages = {
-  name: "fimbrethil"
+const cjs = {
+  input: 'src/index.ts',
+  output: {
+    entryFileNames: '[name].js',
+    dir: 'dist',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+      outDir: './dist',
+      declarationDir: './dist/types',
+      include: [
+        './src/**/*.ts',
+      ],
+      exclude: [
+        './src/**/*.spec.ts',
+      ]
+    })
+  ]
+};
+const es = {
+  ...cjs,
+  output: {
+    ...cjs.output,
+    dir: 'dist/es',
+    format: 'es',
+    sourcemap: true
+  }
 }
 export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      dir: 'dist',
-      format: 'cjs',
-      sourcemap: true
-    },
-    plugins: [
-      typescript({
-        typescript: require('typescript'),
-        outDir: './dist',
-        declarationDir: './dist/types',
-        include: [
-          './src/**/*.ts',
-        ],
-        exclude: [
-          './src/**/*.spec.ts',
-        ]
-      })
-    ]
-  }
+  cjs
 ]
